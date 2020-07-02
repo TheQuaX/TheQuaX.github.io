3fe291d4b6666bff817a6cb0b6048db227d328a6
TheQuaX <alexandermordas@gmail.com>
2020-07-02 11:55:46 +0200

    added twitch.js

--- assets/js/twitch.js
+++
@@ -1,8 +0,0 @@
-$('.followers').each(function() {
-  var nick = $(this).data('nick');
-  var span = $(this).find('span');
-  $.get('https://api.twitch.tv/kraken/streams/' + nick, function(data) {
-    var followers = data.stream.channel.followers;
-    span.html(followers);
-  });
-});
